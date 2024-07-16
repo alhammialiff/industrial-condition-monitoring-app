4d92df4bf6f6430976b302a48ca5e31bc0c4adc3
aliff <alhammi.aliff@stelandsystems.onmicrosoft.com>
2024-07-16 16:30:31 +0800

    [First Commit] Learned a new way of navigating with composable, moved composables within Main Activity to HomeScreen.kt in a bid to modularise app

---
+++ app/src/main/java/com/example/kotlindevcourse/NavGraph.kt
@@ -0,0 +1,37 @@
+package com.example.kotlindevcourse
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ){
+
+        composable(
+            route = Screen.Home.route
+        ){
+
+            HomeScreen()
+
+        }
+
+        composable(
+            route = Screen.Overview.route
+        ){
+
+//            MainActivity()
+
+        }
+
+
+    }
+
+
+}
